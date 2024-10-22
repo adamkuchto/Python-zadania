5c9184cf204fd49365e3d012eb92867973f49a8f
Adam 76864 <gax35606@student.gdynia.merito.pl>
2024-10-22 19:08:39 +0200

    added color printing

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from random import randint
-from colorama import Fore, Back, Style
+from tools import ColorPrint
 import toml
 
 YOUWON = True
@@ -31,16 +31,16 @@ class Player:
 			with open(self.nick + self.path, 'w') as f:
 				toml.dump(text, f)
 		except FileNotFoundError as err:
-			print(f'error: {err}')
+			ColorPrint(ColorPrint.error, f'error: {err}')
 
 
 	def readResult(self):
 		try:
 			with open(self.nick + self.path, 'r') as f:
 				text = toml.load(f)
-				print(text['player'][self.nick])
+				ColorPrint(ColorPrint.error, text['player'][self.nick])
 		except FileNotFoundError as err:
-			print(f'error: {err}')
+			ColorPrint(ColorPrint.error, f'error: {err}')
 
 
 class Game:
@@ -52,20 +52,19 @@ class Game:
 		try:
 			range_value = int(input("Set the range: "))
 			if range_value > 100 or range_value < 0:
-				print("Wrong number, please enter a number between 0 and 100.")
+				ColorPrint(ColorPrint.error, "Wrong number, please enter a number between 0 and 100.")
 			else:
 				self.goldNumber = randint(0, range_value)
 		except ValueError:
-			print("Please enter a valid number.")
+			ColorPrint(ColorPrint.error, "Please enter a valid number.")
 
 	def getPlayerNumber(self):
 		try:
 			self.playerNumber = int(input("Guess the nuber!!!\n\r"))
 		except ValueError:
-			print("Please enter a valid number.")
+			ColorPrint(ColorPrint.error, "Please enter a valid number.")
 
 	def checkNumers(self):
-		print(f"TEST GOLD: {self.goldNumber} PLAYER: {self.playerNumber}")
 		return YOUWON if self.goldNumber == self.playerNumber else YOULOSE
 
 class Menu:
@@ -76,41 +75,40 @@ class Menu:
 		pass
 
 	def printMenu(self):
-		print(Fore.BLACK + Back.GREEN + NOTICE)
-		print(Style.RESET_ALL)
-
-# def main():
-	# menu = Menu()
-	# game = Game()
-	# menu.printMenu()
-	# player = Player(input("Enter your nickname: "))
-	# # sprawdz czy już grał
-	# player.result = 3
-	# game.getGoldNumber()
-	# try:
-	# 	while player.result:
-	# 		player.result -= 1
-	# 		game.getPlayerNumber()
-	# 		if game.checkNumers():
-	# 			print("YOU WON")
-	# 			break
-	# 		else:
-	# 			print("WRONG")
-	# 			continue
-	# except:
-	# 	pass
-	# finally:
-	# 	player.saveResult()
-	# 	print(f"Result saved for player {player.nick} with score {player.result}")
+		ColorPrint(ColorPrint.info, NOTICE)
 
 def main():
 	menu = Menu()
 	game = Game()
 	menu.printMenu()
-	# player = Player("Player1")
-	# player.result = "4"
-	# player.saveResult()
-	# player.readResult()
+	player = Player(input("Enter your nickname: "))
+	# sprawdz czy już grał
+	player.result = 3
+	game.getGoldNumber()
+	try:
+		while player.result:
+			player.result -= 1
+			game.getPlayerNumber()
+			if game.checkNumers():
+				ColorPrint(ColorPrint.error, "YOU WON")
+				break
+			else:
+				ColorPrint(ColorPrint.error, "WRONG")
+				continue
+	except:
+		pass
+	finally:
+		player.saveResult()
+		ColorPrint(ColorPrint.info, f"Result saved for player {player.nick} with score {player.result}")
+
+# def main():
+# 	menu = Menu()
+# 	game = Game()
+# 	menu.printMenu()
+# 	# player = Player("Player1")
+# 	# player.result = "4"
+# 	# player.saveResult()
+# 	# player.readResult()
 
 
 if __name__ == "__main__":
---
+++ tools.py
@@ -0,0 +1,12 @@
+from colorama import Fore, Back, Style
+
+class ColorPrint:
+	error = Fore.WHITE + Back.RED
+	info = Fore.BLACK + Back.GREEN
+	succes = Fore.CYAN + Back.WHITE
+	fail = Fore.BLUE + Back.YELLOW
+
+	def __init__(self, style, text) -> None:
+		print(style + text + Style.RESET_ALL)
+	
+
