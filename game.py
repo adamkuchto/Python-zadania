81d07ec8645973069122538837736ec3f3b26f8b
Adam 76864 <gax35606@student.gdynia.merito.pl>
2024-11-09 12:43:33 +0100

    Refactored after use pylint

--- game.py
+++ game.py
@@ -1,73 +1,117 @@
-from random import randint
+"""
+This file contains all function about the game logic.
+"""
+from random import randint, choice
 from tools import ColorPrint
-import random
 
 YOUWON = True
 YOULOSE = False
 
 class Game:
-	def __init__(self) -> None:
-		self.goldNumber = None
-		self.playerNumber = None
-		self.range = None
+    """
+    A class to represent the game logic.
+    """
+    def __init__(self) -> None:
+        """
+        Initializes an instance of the Game class.
+        """
+        self.goldNumber = None
+        self.playerNumber = None
+        self.range = None
 
-	def getRange(self):
-		try:
-			range_value = int(input("Set the range (0-100): "))
-			if 0 <= range_value <= 100:
-				self.range = range_value
-			else:
-				ColorPrint(ColorPrint.error, "Wrong number, please enter a number between 0 and 100.")
-		except ValueError:
-			ColorPrint(ColorPrint.error, "Please enter a valid number.")
+    def getRange(self):
+        """
+        Prompts the user to set the range for the goldNumber.
+        Ensures the range is between 0 and 100.
+        """
+        try:
+            range_value = int(input("Set the range (0-100): "))
+            if 0 <= range_value <= 100:
+                self.range = range_value
+            else:
+                ColorPrint(ColorPrint.error, "Wrong number,\
+                    please enter a number between 0 and 100.")
+        except ValueError:
+            ColorPrint(ColorPrint.error, "Please enter a valid number.")
 
-	def getGoldNumber(self):
-		if self.range is not None:
-			self.goldNumber = randint(0, self.range)
-		else:
-			ColorPrint(ColorPrint.error, "Range is not set. Please set the range first.")
+    def getGoldNumber(self):
+        """
+        Sets the goldNumber within the specified range.
+        If the range is not set, prompts the user to set the range first.
+        """
+        if self.range is not None:
+            self.goldNumber = randint(0, self.range)
+        else:
+            ColorPrint(ColorPrint.error, "Range is not set. Please set the range first.")
 
-	def getPlayerNumber(self, text="Guess"):
-		try:
-			ColorPrint(ColorPrint.info, f"{text} the number!!!\n\r")
-			self.playerNumber = int(input())
-		except ValueError:
-			ColorPrint(ColorPrint.error, "Please enter a valid number.")
+    def getPlayerNumber(self, text="Guess"):
+        """
+        Prompts the player to guess a number.
+        
+        Parameters
+        ----------
+        text : str
+            The prompt text to display to the player.
+        """
+        try:
+            ColorPrint(ColorPrint.info, f"{text} the number!!!\n\r")
+            self.playerNumber = int(input())
+        except ValueError:
+            ColorPrint(ColorPrint.error, "Please enter a valid number.")
 
-	def checkNumbers(self):
-		return YOUWON if self.goldNumber == self.playerNumber else YOULOSE
+    def checkNumbers(self):
+        """
+        Checks if the player's guess matches the goldNumber.
+        
+        Returns
+        -------
+        bool
+            True if the player's guess matches the goldNumber, False otherwise.
+        """
+        return YOUWON if self.goldNumber == self.playerNumber else YOULOSE
 
-	def playGame(self, attempts, mode=1):
-		self.getRange()
-		if mode == 1:
-			self.getGoldNumber()
-		elif mode == 2:
-			self.getPlayerNumber(text="Set")
-		elif mode == 3:
-			self.getGoldNumber()
+    def playGame(self, attempts, mode=1):
+        """
+        Runs the main game loop with the specified number of attempts and mode.
+        
+        Parameters
+        ----------
+        attempts : int
+            The number of attempts the player has to guess the number.
+        mode : int, optional
+            The mode of the game (default is 1).
+            Mode 1: Player guesses the number.
+            Mode 2: Player sets the number, computer guesses.
+            Mode 3: Alternating turns between player and computer.
+        """
+        self.getRange()
+        if mode == 1:
+            self.getGoldNumber()
+        elif mode == 2:
+            self.getPlayerNumber(text="Set")
+        elif mode == 3:
+            self.getGoldNumber()
 
-		playerTurn = random.choice([True, False])
+        playerTurn = choice([True, False])
 
-		while attempts:
-			if mode == 1 or (mode == 3 and playerTurn):
-				ColorPrint(ColorPrint.info, "Player turn")
-				self.getPlayerNumber()
-			else:
-				ColorPrint(ColorPrint.info, "Computer turn")
-				self.getGoldNumber()
+        while attempts:
+            if mode == 1 or (mode == 3 and playerTurn):
+                ColorPrint(ColorPrint.info, "Player turn")
+                self.getPlayerNumber()
+            else:
+                ColorPrint(ColorPrint.info, "Computer turn")
+                self.getGoldNumber()
 
+            if self.checkNumbers():
+                if mode == 1 or (mode == 3 and playerTurn):
+                    ColorPrint(ColorPrint.success, "YOU WON")
+                else:
+                    ColorPrint(ColorPrint.success, "COMPUTER WON")
+                break
+            else:
+                attempts -= 1
+                ColorPrint(ColorPrint.error, "WRONG")
+                if mode == 3:
+                    playerTurn = not playerTurn
 
-			if self.checkNumbers():
-				if mode == 1 or (mode == 3 and playerTurn):
-					ColorPrint(ColorPrint.success, "YOU WON")
-				else:
-					ColorPrint(ColorPrint.success, "COMPUTER WON")
-				break
-			else:
-				attempts -= 1
-				ColorPrint(ColorPrint.error, "WRONG")
-				if mode == 3:
-					playerTurn = not playerTurn
-		else:
-			ColorPrint(ColorPrint.error, "GAME OVER")
-
+        ColorPrint(ColorPrint.error, "GAME OVER")
