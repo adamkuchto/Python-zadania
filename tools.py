1ea8168de490ce907d0a0d091f03af87d1ffa9cd
Adam 76864 <gax35606@student.gdynia.merito.pl>
2024-11-09 12:49:07 +0100

    Refactored after use pylint

--- tools.py
+++ tools.py
@@ -1,12 +1,26 @@
+"""
+Additional tools. 
+"""
 from colorama import Fore, Back, Style
 
 class ColorPrint:
-	error = Fore.WHITE + Back.RED
-	info = Fore.BLACK + Back.GREEN
-	success = Fore.CYAN + Back.WHITE
-	fail = Fore.BLUE + Back.YELLOW
+    """
+    A class to print colored text using the colorama library.
+    """
+    error = Fore.WHITE + Back.RED
+    info = Fore.BLACK + Back.GREEN
+    success = Fore.CYAN + Back.WHITE
+    fail = Fore.BLUE + Back.YELLOW
 
-	def __init__(self, style, text) -> None:
-		print(style + text + Style.RESET_ALL)
-	
+    def __init__(self, style, text) -> None:
+        """
+        Initializes the ColorPrint instance and prints the text with the specified style.
 
+        Parameters
+        ----------
+        style : str
+            The style to apply to the text.
+        text : str
+            The text to be printed.
+        """
+        print(style + text + Style.RESET_ALL)
